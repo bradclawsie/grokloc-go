87885ad0bd40c1a343efb25a3708b3263dd56c39
Brad Clawsie <brad@b7j0c.org>
2025-11-20 13:04:00 -0600

    init crypt and models pkgs

---
+++ pkg/model/status/status.go
@@ -0,0 +1,10 @@
+/*
+Package status provides model status constants that
+map to database values.
+*/
+package status
+
+const Unconfirmed = 1
+const Active = 2
+const Inactive = 3
+
---
+++ pkg/security/password/password.go
@@ -0,0 +1,22 @@
+/*
+Package password contains Argon2 utilities.
+*/
+package password
+
+import (
+	"github.com/matthewhartstonge/argon2"
+)
+
+// Encode performs a one-way hash on a password using argon2.
+func Encode(password string, cfg argon2.Config) (string, error) {
+	raw, err := cfg.Hash([]byte(password), nil)
+	if err != nil {
+		return "", err
+	}
+	return string(raw.Encode()), err
+}
+
+// Verify returns true if guess is the same as encoded.
+func Verify(guess string, encoded string) (bool, error) {
+	return argon2.VerifyEncoded([]byte(guess), []byte(encoded))
+}
